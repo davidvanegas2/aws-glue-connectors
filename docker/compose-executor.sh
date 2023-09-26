1367bb46efcaef378cf455e6eec75d09d361ea36
David Felipe Vanegas Ramirez <david@loka.com>
2023-09-26 14:55:17 -0500

    feat: Remove TODO comments

--- docker/compose-executor.sh
+++ docker/compose-executor.sh
@@ -35,25 +35,22 @@ if ! [ -x ".jupyter_lab/stop_service.sh" ]; then
     chmod +x ./jupyter_lab/stop_service.sh
 fi
 
-# TODO: Uncomment the line below when the Spark Submit is ready
-#if ! [ -x ".spark_submit/stop_service.sh" ]; then
-#    chmod +x ./spark_submit/stop_service.sh
-#fi
+if ! [ -x ".spark_submit/stop_service.sh" ]; then
+    chmod +x ./spark_submit/stop_service.sh
+fi
 
 # If the parameter is jupyter-lab, it will run the file: "jupyter_lab/env-setup-and-run.sh".
 # If the parameter is spark-submit, it will run the file: "spark_submit/env-setup-and-run.sh"
 if [ "$1" == "jupyter-lab" ]; then
     echo "Running Jupyter Notebook"
     ./jupyter_lab/env-setup-and-run.sh
-# TODO: Uncomment the line below when the Spark Submit is ready
-#elif [ "$1" == "spark-submit" ]; then
-#    echo "Running Spark Submit"
-#    ./spark_submit/env-setup-and-run.sh
+elif [ "$1" == "spark-submit" ]; then
+    echo "Running Spark Submit"
+    ./spark_submit/env-setup-and-run.sh
 elif [ "$1" == "stop" ]; then
     echo "Stopping all services"
     ./jupyter_lab/stop_service.sh
-    # TODO: Uncomment the line below when the Spark Submit is ready
-#    ./spark_submit/stop_service.sh
+    ./spark_submit/stop_service.sh
 else
     echo "Invalid argument. Please provide the service you want to run: jupyter-lab or spark-submit"
     exit 1
